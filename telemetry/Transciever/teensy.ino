0b1890c39ff6e29b4c911c52da3678c0b3ee4ec9
Thomas-D-Pitt <54122854+Thomas-D-Pitt@users.noreply.github.com>
2024-02-06 20:52:06 -0500

    Create teensy.ino

---
+++ telemetry/Transciever/teensy.ino
@@ -0,0 +1,38 @@
+#include "sbus.h"
+
+/* SBUS object, reading SBUS */
+bfs::SbusRx sbus_rx(&Serial2);
+/* SBUS object, writing SBUS */
+bfs::SbusTx sbus_tx(&Serial2);
+/* SBUS data */
+bfs::SbusData data;
+
+void setup() {
+  /* Serial to display data */
+  Serial.begin(115200);
+  while (!Serial) {}
+  /* Begin the SBUS communication */
+  sbus_rx.Begin();
+  sbus_tx.Begin();
+  Serial.println("Started...");
+}
+
+void loop () {
+  if (sbus_rx.Read()) {
+    /* Grab the received data */
+    data = sbus_rx.data();
+    /* Display the received data */
+    for (int8_t i = 0; i < data.NUM_CH; i++) {
+      Serial.print(data.ch[i]);
+      Serial.print("\t");
+    }
+    /* Display lost frames and failsafe data */
+    Serial.print(data.lost_frame);
+    Serial.print("\t");
+    Serial.println(data.failsafe);
+    /* Set the SBUS TX data to the received data */
+    sbus_tx.data(data);
+    /* Write the data to the servos */
+    sbus_tx.Write();
+  }
+}
